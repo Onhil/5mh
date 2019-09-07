9258bddf1058b49fda86757959ef4ec96800bca8
Onhil <onhilx@gmail.com>
2019-09-07 14:12:18 +0200

    commit

---
+++ Assets/scripts/AccelIncrease.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelIncrease : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D p) {
+        if (p.tag == "Player") {
+            Player player = p.GetComponent<Player>();
+            if (player != null) player.accelIncrease();
+        }
+    }
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    public double speed, jump, acceleration, accelerationIncrease, maxTime;
+    Transform rayGround;
+    Rigidbody2D rigidBody;
+    float startTime;
+    float timeNow;
+    Text text;
+
+    private float TimeResetX, TimeResetY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        startTime = Time.time;
+        TimeResetX = transform.position.x;
+        TimeResetY = transform.position.y;
+        text = GameObject.Find("Canvas").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeNow = Time.time - startTime;
+        updateTimeLeft();
+        movment();
+        if (Input.GetKeyDown(KeyCode.E)) {
+            transform.position = new Vector3(TimeResetX, TimeResetY, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.F)) {
+            acceleration += 5;
+        }
+        
+    }
+
+    void updateTimeLeft() {
+        float timeLeft = (float)maxTime - timeNow;
+        TimeSpan span = TimeSpan.FromSeconds((double)(new decimal(timeLeft)));
+        text.text = span.ToString();
+    }
+
+    void movment() 
+    {
+        speed = Input.GetAxis("Horizontal") * acceleration;
+        Vector2 direction = new Vector2((float)speed, 0);
+        rigidBody.AddForce(direction);
+    }
+
+    public void accelIncrease() {
+        acceleration *= (1 + accelerationIncrease);
+    }
+
+}
